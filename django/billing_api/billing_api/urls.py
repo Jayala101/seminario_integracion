c99fe9248d732de86ff0a0801eda5e76e09d2980
Jayala101 <jimmysanti1@hotmail.com>
2025-10-29 13:04:52 -0500

    views category modified

--- django/billing_api/billing_api/urls.py
+++ django/billing_api/billing_api/urls.py
@@ -1,7 +1,9 @@
+#billing_api/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path('api/', include('catalog.urls')), 
 ]
