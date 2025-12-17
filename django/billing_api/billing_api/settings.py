bcd0cedb27150c39e07f7905de60ddee424fe7db
Jayala101 <jimmysanti1@hotmail.com>
2025-12-16 19:59:09 -0500

    added payments in mongodb

--- django/billing_api/billing_api/settings.py
+++ django/billing_api/billing_api/settings.py
@@ -15,7 +15,7 @@ INSTALLED_APPS = [
   'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
   'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
   'rest_framework','django_filters',
-  'users','catalog','invoices','basics','warehouses',
+  'users','catalog','invoices','basics','warehouses','payments',
 ]
 
 MIDDLEWARE = [
